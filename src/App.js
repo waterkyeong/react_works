b0de24b9ca9e9ab464e66070484d1e721bb7f61f
waterkyeong <su5368@naver.com>
2024-05-03 17:57:49 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import { HiMiniHome } from "react-icons/hi2";
 function App() {
   return (
     <div className="flex flex-col w-full max-w-screen-lg h-screen overflow-y-auto mx-auto">
-      <header className='flex justify-between items-center text-xl font-bold h-20 p-10 bg-green-200 text-lime-950'>
-        <p className='text-3xl'>Header</p>
-        <p className='text-2xl'>K-digital</p>
+      <header className='flex justify-between items-center text-xl font-bold h-20 p-10 bg-green-200 text-emerald-800'>
+        <p className='text-xl'>React training</p>
+        <p className='text-5xl'>K-digital</p>
         <p className='text-3xl'><HiMiniHome /></p>
       </header>
        <main className='grow'>
         main
        </main>
-       <footer className='flex justify-center items-center text-white bg-emerald-900 h-20'>
+       <footer className='flex justify-between items-center text-white bg-emerald-900 h-20'>
         footer
        </footer>
     </div>
